1d8e850db84bbd614ba1f0f827adf419ac9be022
Joe Wigdor <joe@wigdor.io>
2024-07-13 12:06:22 -0700

    Added value range mapping for graph

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
         perlinTest();
-        //geographyTest();
-    }
-
-    public static void geographyTest() {
-        GeographyGenerator testGenerator = new GeographyGenerator(12345);
     }
 
     public static void perlinTest() {
@@ -16,7 +13,22 @@ public class Main {
         windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windowFrame.setSize(1024,1024);
         Random newSeed = new Random();
-        windowFrame.add(new PerlinGraph(512,512, 32768, 0.1, 2, 6, 0.4));
+
+        ArrayList<ArrayList<Integer>> testRanges = new ArrayList<>();
+        // Each range consists of a perlin value range for the first two values and four
+        // RGBA8 values describing the desired mapped color
+        testRanges.add(new ArrayList<>(Arrays.asList(0,31,0,0,0,255)));
+        testRanges.add(new ArrayList<>(Arrays.asList(32,63,255,0,255,255)));
+
+        testRanges.add(new ArrayList<>(Arrays.asList(64,95,255,0,0,255)));
+        testRanges.add(new ArrayList<>(Arrays.asList(96,127,0,255,0,255)));
+        testRanges.add(new ArrayList<>(Arrays.asList(128,159,0,0,255,255)));
+        testRanges.add(new ArrayList<>(Arrays.asList(160,191,255,255,0,255)));
+
+        testRanges.add(new ArrayList<>(Arrays.asList(192,223,0,255,255,255)));
+        testRanges.add(new ArrayList<>(Arrays.asList(224,255,255,255,255,255)));
+
+        windowFrame.add(new PerlinGraph(512,512, 12346, 0.1, 2, 6, 0.4, testRanges));
         windowFrame.setVisible(true);
     }
 }
--- src/PerlinGraph.java
+++ src/PerlinGraph.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PerlinGraph extends JPanel {
     private final Integer[][] perlinMatrix;
+    private final Color[][] graphMatrix;
 
-    PerlinGraph(int width, int height, int randSeed, double depth, double scale, int octaves, double persistence) {
+    PerlinGraph(int width, int height, int randSeed, double depth, double scale, int octaves, double persistence,
+                ArrayList<ArrayList<Integer>> colorRanges) {
         PerlinNoise graphNoise = new PerlinNoise(4,256,2, randSeed);
         perlinMatrix = new Integer[width][height];
+        graphMatrix = new Color[width][height];
 
         for (int y = 0; y < width; y++) {
             for (int x = 0; x < height; x++) {
@@ -16,21 +20,18 @@ public class PerlinGraph extends JPanel {
                 //double perlinVal = graphNoise.perlinHalf(px * scale, py * scale, depth * scale); // Single layer of perlin noise
                 double perlinVal = graphNoise.octavePerlinHalf(px * scale, py * scale, depth * scale, octaves, persistence);
                 int matrixVal = doubleToInt(perlinVal);
+                perlinMatrix[x][y] = matrixVal;
 
                 // Clamps
 
-                if (matrixVal > 0 && matrixVal < 120)
-                    matrixVal = 255;
-                else if (matrixVal >= 120 && matrixVal < 150)
-                    matrixVal = 128;
-                else if (matrixVal > 190 && matrixVal < 255)
-                    matrixVal = 200;
-                //else if (matrixVal > 127 && matrixVal < 137)
-                //    matrixVal = 255;
-                else
-                    matrixVal = 0;
-
-                perlinMatrix[x][y] = matrixVal;
+                for (ArrayList<Integer> curRange : colorRanges) {
+                    if (matrixVal > curRange.get(0) && matrixVal < curRange.get(1)) {
+                        graphMatrix[x][y] = new Color(curRange.get(2),
+                                curRange.get(3),
+                                curRange.get(4),
+                                curRange.get(5));
+                    }
+                }
             }
         }
     }
@@ -49,11 +50,15 @@ public class PerlinGraph extends JPanel {
         return perlinMatrix;
     }
 
+    public Color[][] getGraphMatrix() {
+        return graphMatrix;
+    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        int pWidth = perlinMatrix[0].length;
-        int pHeight = perlinMatrix.length;
+        int pWidth = graphMatrix[0].length;
+        int pHeight = graphMatrix.length;
         int wWidth = this.getWidth();
         int wHeight = this.getHeight();
 
@@ -64,15 +69,13 @@ public class PerlinGraph extends JPanel {
 
                 for (int y = 0; y < pHeight; y++) {
                     for (int x = 0; x < pWidth; x++) {
-                        int curColor = perlinMatrix[x][y];
-                        g2d.setColor(new Color(curColor,curColor,curColor));
+                        Color curColor = graphMatrix[x][y];
+                        g2d.setColor(curColor);
                         g2d.drawLine(x+xOffset,y+yOffset,x+xOffset,y+yOffset);
                     }
                 }
-
             }
         }
-
     }
 }
 
